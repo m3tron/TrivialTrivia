4cc776779f63b5dccc8fbb73a49f2e2d13267d09
Sunny Heyar <sunny.heyar@gmail.com>
2020-04-11 21:36:48 -0400

    adjusted package.json on server side

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
